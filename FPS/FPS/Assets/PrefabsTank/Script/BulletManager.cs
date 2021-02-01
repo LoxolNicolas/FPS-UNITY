a33e68878029f4df6f66237213199a320b4114ea
unknown <guillaume.livet@etu.uca.fr>
2021-02-01 17:56:03 +0100

    ajout du systeme de tirs au tanks

---
+++ FPS/FPS/Assets/PrefabsTank/Script/BulletManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BulletManager : MonoBehaviour
+{
+    public GameObject Player;
+    public int BulletDamage;
+    [SerializeField]
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<Rigidbody>().AddForce(transform.forward * 3000);
+        StartCoroutine(DestroyBullet());
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+    
+    public void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("cc");
+        if((collision.gameObject.CompareTag("Team1") && Player.CompareTag("Team2")) || (collision.gameObject.CompareTag("Team2") && Player.CompareTag("Team1"))){
+            collision.gameObject.GetComponent<PlayerLogic>().TakeDamage(BulletDamage);
+            Debug.Log("erqover");
+        }
+        Destroy(gameObject);
+    }
+    
+}
