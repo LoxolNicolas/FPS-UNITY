e3512de36950e80ff4d18f285b6f573d7047525d
LoxolNicolas <33003475+LoxolNicolas@users.noreply.github.com>
2021-03-28 21:24:17 +0200

    Ajout du projet concernant les bonus

---
+++ Bonus/Assets/PrefabsTank/Script/TankSetup.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class TankSetup : NetworkBehaviour
+{
+    [SerializeField]
+    private Behaviour[] componentsToDisable;
+
+    private Camera sceneCamera;
+
+    void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+    }
+
+    void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
